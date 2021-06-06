04f60087027894d30de0775adfae6e22322485f9
Willian Aquino <aquino.will@gmail.com>
2021-06-06 11:53:22 -0300

    New images app and corretly build app

--- lib/components/adaptative_date_picker.dart
+++ lib/components/adaptative_date_picker.dart
@@ -5,38 +5,38 @@ import 'package:flutter/cupertino.dart';
 
 class AdaptativeDatePicker extends StatelessWidget {
   final DateTime selectedDate;
-  final Function (DateTime) onDateChange;
+  final Function(DateTime) onDateChange;
 
-  AdaptativeDatePicker({ this.selectedDate, this.onDateChange })
+  AdaptativeDatePicker({this.selectedDate, this.onDateChange});
 
   @override
   Widget build(BuildContext context) {
     _showDatePicker(BuildContext context) {
-    showDatePicker(
-            context: context,
-            initialDate: DateTime.now(),
-            firstDate: DateTime(2019),
-            lastDate: DateTime.now())
-        .then((pickedDate) {
-      if (pickedDate == null) {
-        return;
-      }
+      showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime(2019),
+              lastDate: DateTime.now())
+          .then((pickedDate) {
+        if (pickedDate == null) {
+          return;
+        }
 
-      onDateChange(pickedDate);
-    });
-  }
+        onDateChange(pickedDate);
+      });
+    }
 
     return Platform.isIOS
         ? Container(
-          height: 180,
-          child: CupertinoDatePicker(
-            mode: CupertinoDatePickerMode.date,
-            initialDateTime: DateTime.now(),
-            minimumDate: DateTime(2019),
-            maximumDate: DateTime.now(),
-            onDateTimeChanged: onDateChange,
-          ),
-        )
+            height: 180,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: DateTime.now(),
+              minimumDate: DateTime(2019),
+              maximumDate: DateTime.now(),
+              onDateTimeChanged: onDateChange,
+            ),
+          )
         : Container(
             child: Row(children: [
             Text(selectedDate == null
